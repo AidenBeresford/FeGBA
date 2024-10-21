40f477fe5289067cfbadeb007f1de5f206199ae1
Aiden Beresford <aiden_beresford@tamu.edu>
2024-10-21 17:58:52 -0500

    Add branch and branch with link, + misc. changes

--- src/core/dissassembler.rs
+++ src/core/dissassembler.rs
@@ -1,8 +1,17 @@
-pub fn is_branch_and_branch_exchange(opcode: u32) -> bool {
-    const BRANCH_AND_EXCHANGE_FORMAT: u32 = 0b0000_0001_0010_1111_1111_1111_0001_0000;
-    const FORMAT_MASK: u32 = 0b0000_1111_1111_1111_1111_1111_1111_0000;
 
-    let extracted_format = opcode & FORMAT_MASK;
+fn arm_branch_and_branch_exchange(opcode: u32) -> bool {
+    let format: u32 = 0b0000_0001_0010_1111_1111_1111_0001_0000;
+    let mask: u32 = 0b0000_1111_1111_1111_1111_1111_1111_0000;
+    
+    (opcode & mask) == format
+}
 
-    extracted_format == BRANCH_AND_EXCHANGE_FORMAT
+// block data transfer here
+
+fn arm_branch_and_branch_with_link(opcode: u32) -> bool {
+    let format: u32 = 0b0000_1010_0000_0000_0000_0000_0000_0000;
+    let link: u32 = 0b0000_1011_0000_0000_0000_0000_0000_0000;
+    let mask: u32 = 0b0000_1111_0000_0000_0000_0000_0000_0000;
+
+    (opcode & mask) == format || (opcode & mask) == link // could we not just mask with 0b0000_1110_...? someone check this please
 }
