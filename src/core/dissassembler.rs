07ac1d709e31d90145e0c9ee3a89bc7144674ea9
Aiden Beresford <aiden_beresford@tamu.edu>
2024-10-21 19:44:16 -0500

    Add disassembler enums and pub function, add software interrupt and undefined

--- src/core/dissassembler.rs
+++ src/core/dissassembler.rs
@@ -1,3 +1,57 @@
+enum InstructionSet {
+    ARM(opcode),
+    THUMB(opcode)
+}
+
+enum Instruction {
+    BranchAndBranchExchange,
+    BlockDataTransfer,
+    BranchAndBranchWithLink,
+    SoftwareInterruptA,
+    Undefined,
+    SingleDataTransfer,
+    SingleDataSwap,
+    MultiplyAndMultiplyLong,
+    HalfwordDataTransfer,
+    PSRTransfer,
+    DataProcessing, // END OF ARM
+    SoftwareInterruptT,
+    UncoditionalBranch,
+    ConditionalBranch,
+    MultipleLoadStore,
+    LongBranchWithLink,
+    AddOffsetToSP,
+    PushPopRegister,
+    LoadStoreHalfword,
+    SPRelativeLoadStore,
+    LoadAddress,
+    LoadStoreImmediateOffset,
+    LoadStoreRegisterOffset,
+    LoadStoreSignExtended,
+    PCRelativeLoad,
+    HiRegisterOperation,
+    ALUOperation,
+    MoveCompareAddSubImmediate,
+    AddSubtract,
+    MoveShiftedRegister
+}
+
+pub fn disassemble(inset: InstructionSet) -> Instruction {
+    match {
+        InstructionSet::ARM(op) => return disassemble_arm(op),
+        InstructionSet::THUMB(op) => return disassemble_thumb(op),
+    }
+}
+
+// TODO
+fn disassemble_arm() -> Instruction {
+    Instruction::SoftwareInterruptA // placeholder
+}
+
+// TODO
+fn disassemble_thumb() -> Instruction {
+    Instruction::SoftwareInterruptT // placeholder
+}
 
 fn arm_branch_and_branch_exchange(opcode: u32) -> bool {
     let format: u32 = 0b0000_0001_0010_1111_1111_1111_0001_0000;
@@ -13,5 +67,19 @@ fn arm_branch_and_branch_with_link(opcode: u32) -> bool {
     let link: u32 = 0b0000_1011_0000_0000_0000_0000_0000_0000;
     let mask: u32 = 0b0000_1111_0000_0000_0000_0000_0000_0000;
 
-    (opcode & mask) == format || (opcode & mask) == link // could we not just mask with 0b0000_1110_...? someone check this please
+    (opcode & mask) == format || (opcode & mask) == link 
+}
+
+fn arm_software_interrupt(opcode: u32) -> bool {
+    let format: u32 = 0b0000_1111_0000_0000_0000_0000_0000_0000;
+    let mask: u32 = 0b0000_1111_0000_0000_0000_0000_0000_0000;
+
+    (opcode & mask) == format
+}
+
+fn arm_undefined(opcode: u32) -> bool {
+    let format: u32 = 0b0000_0110_0000_0000_0000_0000_0001_0000;
+    let mask: u32 = 0b0000_1110_0000_0000_0000_0000_0001_0000;
+
+    (opcode & mask) == format
 }
