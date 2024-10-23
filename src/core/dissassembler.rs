2951951b7cd75f0b39ac997137b55e36b394cf83
claytonsmith1027 <claytonsmith1027@tamu.edu>
2024-10-22 21:36:43 -0500

    add remaining arm disassemblies

--- src/core/dissassembler.rs
+++ src/core/dissassembler.rs
@@ -6,3 +6,67 @@ pub fn is_branch_and_branch_exchange(opcode: u32) -> bool {
 
     extracted_format == BRANCH_AND_EXCHANGE_FORMAT
 }
+
+fn arm_block_data_transfer(opcode: u32) -> bool {
+    let format: u32 = 0b0000_1000_0000_0000_0000_0000_0000_0000;
+    let mask: u32 = 0b0000_1110_0000_0000_0000_0000_0000_0000;
+
+    (opcode & mask) == format
+}
+
+fn arm_single_data_transfer(opcode: u32) -> bool {
+    let format: u32 = 0b0000_0100_0000_0000_0000_0000_0000_0000;
+    let mask: u32 = 0b0000_1100_0000_0000_0000_0000_0000_0000;
+
+    (opcode & mask) == format
+}
+
+fn arm_single_data_swap(opcode: u32) -> bool {
+    let format: u32 = 0b0000_0001_0000_0000_0000_0000_1001_0000;
+    let mask: u32 = 0b0000_1111_1000_0000_0000_1111_1111_0000;
+
+    (opcode & mask) == format
+}
+
+fn arm_multiply_and_multiply_long(opcode: u32) -> bool {
+    let multiply_format: u32 = 0b0000_0000_0000_0000_0000_0000_1001_0000;
+    let multiply_long_format: u32 = 0b0000_0000_1000_0000_0000_0000_1001_0000;
+    let mask: u32 = 0b0000_1111_1000_0000_0000_0000_1111_0000;
+
+    (opcode & mask) == multiply_format || (opcode & mask) == multiply_long_format 
+}
+
+fn arm_halfword_data_transfer_register(opcode: u32) -> bool {
+    let format: u32 = 0b0000_0000_0000_0000_0000_0000_1001_0000;
+    let mask: u32 = 0b0000_1110_0100_0000_0000_1111_1001_0000;
+
+    (opcode & mask) == format
+}
+
+fn arm_halfword_data_transfer_immediate(opcode: u32) -> bool {
+    let format: u32 = 0b0000_0000_0100_0000_0000_0000_1001_0000;
+    let mask: u32 = 0b0000_1110_0100_0000_0000_0000_1001_0000;
+
+    (opcode & mask) == format
+}
+
+fn arm_psr_transfer_mrs(opcode: u32) -> bool {
+    let format: u32 = 0b0000_0001_0000_1111_0000_0000_0000_0000;
+    let mask: u32 = 0b0000_1111_1011_1111_0000_0000_0000_0000;
+
+    (opcode & mask) == format
+}
+
+fn arm_psr_transfer_msr(opcode: u32) -> bool {
+    let format: u32 = 0b0000_0001_0010_0000_1111_0000_0000_0000;
+    let mask: u32 = 0b0000_1101_1011_0000_1111_0000_0000_0000;
+
+    (opcode & mask) == format
+}
+
+fn arm_data_processing(opcode: u32) -> bool {
+    let format: u32 = 0b0000_0000_0000_0000_0000_0000_0000_0000;
+    let mask: u32 = 0b0000_1100_0000_0000_0000_0000_0000_0000;
+
+    (opcode & mask) == format
+}
