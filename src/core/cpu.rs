3c3325ba5f5dc92e44c86a39623eac90d40f8c29
Aiden Beresford <aiden_beresford@tamu.edu>
2024-10-28 18:44:00 -0500

    Update cpu.rs

--- src/core/cpu.rs
+++ src/core/cpu.rs
@@ -8,18 +8,18 @@ use crate::constants::condition_codes;
 
 pub struct ARM7TDMI {
     // register indexes are as follows
-    // low regs are 0-7
-    // high general-purpose regs user/system are 8-12
-    // fiq registers are 20-26
+    // low regs are 0-7 inclusive
+    // usr/sys high general-purpose regs are 8-12 inclusive
+    // fiq high general-purpose regs are 20-24 inclusive
     register: [u32; 37],
-    mode_register: [usize; 17], // this array is of indexes for register
+    idx: [usize; 17], // this array is of indexes for register
 }
 
 impl Default for ARM7TDMI {
     fn default() -> ARM7TDMI {
         let mut cpu = ARM7TDMI { 
             register: [0; 37],
-            mode_register: [0, 1, 2, 3, 4, 5, 6, 7,
+            idx: [0, 1, 2, 3, 4, 5, 6, 7,
             8, 9, 10, 11, 12, 
             register_index::SP_USR, 
             register_index::LR_USR, 
@@ -34,6 +34,49 @@ impl Default for ARM7TDMI {
         cpu.register[34] = register_initial::SP_UND;
 
         cpu
+    }
+}
+
+impl ARM7TDMI {
+    fn set_flag(&mut self, flag: Flag, bit: bool) {
+        let mask = flag.get_mask();
+        if bit == true {
+            self.register[16] |= mask;
+        } else {
+            self.register[16] &= !mask;
+        }
+    }
+
+    fn get_flag(&self, flag: Flag) -> bool {
+        let mask = flag.get_mask();
+        (self.register[16] & mask) != 0
+    }
+
+    fn pass_condition(&self, opcode: u32) -> bool {
+        let condition = opcode & CONDITION_MASK;
+        let n = get_flag(self.register[16], Flag::N);
+        let z = get_flag(self.register[16], Flag::Z);
+        let c = get_flag(self.register[16], Flag::C);
+        let v = get_flag(self.register[16], Flag::V);
+    
+        match condition {
+            condition_codes::EQ => z,
+            condition_codes::NQ => !z,
+            condition_codes::CS_HS => c,
+            condition_codes::CC_LO => !c,
+            condition_codes::MI => n,
+            condition_codes::PL => !n,
+            condition_codes::VS => v,
+            condition_codes::VC => !v,
+            condition_codes::HI => c && !z,
+            condition_codes::LS => !c && z,
+            condition_codes::GE => n == v,
+            condition_codes::LT => n != v,
+            condition_codes::GT => !z && (n == v),
+            condition_codes::LE => z && (n != v),
+            condition_codes::AL => true,
+            _ => false,
+        }
     }
 }
 
@@ -58,45 +101,9 @@ impl Flag {
 }
 
 /// TODO: cpsr hould be replaced with the actual instance vairable 
-fn set_flag(cpsr: &mut u32, flag: Flag, bit: bool) {
-    let mask = flag.get_mask();
-    if bit == true {
-        *cpsr |= mask;
-    } else {
-        *cpsr &= !mask;
-    }
-}
+
 
 /// TODO: cpsr should be replaced with the actual instance vairable 
-fn get_flag(cpsr: u32, flag: Flag) -> bool {
-    let mask = flag.get_mask();
-    (cpsr & mask) != 0
-}
+
 
 /// TODO: cpsr should be replaced with the actual instance vairable 
-fn pass_condition(opcode: u32, cpsr: u32) -> bool {
-    let condition = opcode & CONDITION_MASK;
-    let n = get_flag(cpsr, Flag::N);
-    let z = get_flag(cpsr, Flag::Z);
-    let c = get_flag(cpsr, Flag::C);
-    let v = get_flag(cpsr, Flag::V);
-
-    match condition {
-        condition_codes::EQ => z,
-        condition_codes::NQ => !z,
-        condition_codes::CS_HS => c,
-        condition_codes::CC_LO => !c,
-        condition_codes::MI => n,
-        condition_codes::PL => !n,
-        condition_codes::VS => v,
-        condition_codes::VC => !v,
-        condition_codes::HI => c && !z,
-        condition_codes::LS => !c && z,
-        condition_codes::GE => n == v,
-        condition_codes::LT => n != v,
-        condition_codes::GT => !z && (n == v),
-        condition_codes::LE => z && (n != v),
-        condition_codes::AL => true,
-        _ => false,
-    }
-}
